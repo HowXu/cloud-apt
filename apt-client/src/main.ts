e33d15ba601150f7efede6f50f3f5f25e400b46e
HowXu <dev@howxu.cn>
2026-09-16 18:25:38 +0800

    feat(client): inject <title> and favicon from siteConfig

--- apt-client/src/main.ts
+++ apt-client/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import { siteConfig } from './site.config';
 import 'virtual:uno.css';
 import '@unocss/reset/tailwind.css';
 
@@ -8,6 +9,15 @@ import HomePage from './pages/HomePage.vue';
 import BrowsePage from './pages/BrowsePage.vue';
 import PackagePage from './pages/PackagePage.vue';
 import SearchPage from './pages/SearchPage.vue';
+
+document.title = siteConfig.browserTabTitle || siteConfig.headerTitle;
+if (siteConfig.iconUrl) {
+    const link = document.querySelector<HTMLLinkElement>("link[rel='icon']")
+        ?? document.createElement('link');
+    link.rel = 'icon';
+    link.href = siteConfig.iconUrl;
+    if (!link.isConnected) document.head.appendChild(link);
+}
 
 const router = createRouter({
     history: createWebHistory(),
